68484a7ae39a821d360e25e0902714cc3723483b
littleghosty <yeedaw@live.cn>
2016-10-01 15:45:29 +0800

    create article

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Block, Article
 
 
@@ -12,5 +12,19 @@ def article_list(request, block_id):
 def create_article(request, block_id):
     block_id = int(block_id)
     block = Block.objects.get(id=block_id)
-
-    return render(request, "create_article.html", {"b": block, })
+    if request.method == "GET":
+        return render(request, "create_article.html", {"b": block, })
+    else:
+        title = request.POST["title"].strip()
+        content = request.POST["content"].strip()
+        if not title or not content:
+            return render(request, "create_article.html", {"b": block,
+                                "error": "标题和内容不能为空.", "title": title,
+                                "content": content})
+        if len(title) > 100 or len(content) > 10000:
+            return render(request, "create_article.html", {"b": block,
+                                   "error": "标题或内容太长了。",
+                                   "title": title, "content": content})
+        article = Article(block=block, title=title, content=content, status=0)
+        article.save()
+        return redirect("/article/list/%s" % block_id)
