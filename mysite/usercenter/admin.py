89ffea2b125f98c23a7961e3412a0faf536ebd20
littleghosty <yeedaw@live.cn>
2016-10-16 23:23:18 +0800

    userprofile

--- mysite/usercenter/admin.py
+++ mysite/usercenter/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ActivateCode, UserProfile
+
+
+class ActivateCodeAdmin(admin.ModelAdmin):
+    list_display = ("owner", "code", "expire_timestamp", "last_update_timestamp")
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "sex", "birthday")
+    list_filter = ("sex", )
+
+
+admin.site.register(ActivateCode, ActivateCodeAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
--- mysite/usercenter/models.py
+++ mysite/usercenter/models.py
@@ -3,7 +3,13 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-    owner = models.ForeignKey(User, verbose_name="用户")
+    user = models.OneToOneField(User,
+            on_delete=models.CASCADE)
+    sex = models.IntegerField("性别",
+            choices=((0, "男"), (-1, "女")),
+            default=0)
+    birthday = models.DateTimeField("生日",
+            null=True, blank=True)
 
 
 class ActivateCode(models.Model):
